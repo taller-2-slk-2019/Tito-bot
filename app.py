5582a0370b6f02ecc96471b0220fdae7bdb24edb
matias feld <matias.feld@mdevz.com>
2019-04-23 19:41:30 -0300

    initialized controller

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from controllers.BotController import BotController
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Bot Tito'
+
+
+@app.route('/bot', methods=["POST"])
+def bot():
+    data = request.get_json()
+    result = BotController().analyze_message(data)
+    return str(result)
 
 
 if __name__ == '__main__':
---
+++ controllers/BotController.py
@@ -0,0 +1,4 @@
+class BotController:
+
+    def analyze_message(self, message):
+        return message['data']
