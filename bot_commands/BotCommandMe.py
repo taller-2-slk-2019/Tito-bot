8801322eba85e0def7221dc4b64a66cb116b5057
matias feld <matias.feld@mdevz.com>
2019-04-27 12:55:23 -0300

    implemented 'me' command

--- bot_commands/BotCommandMe.py
+++ bot_commands/BotCommandMe.py
@@ -1,5 +1,16 @@
+from dateutil.parser import *
+from controllers.ServerController import ServerController
+
+
 class BotCommandMe:
 
     def apply(self, params, channel_id, sender_id):
-        # todo
-        return
+        user = ServerController().get_user(sender_id)
+        result = ''
+
+        result += f'Usuario: {user["username"]}\n'
+        result += f'Nombre: {user["name"]}\n'
+        result += f'Email: {user["email"]}\n'
+        result += f'Se unió el {parse(user["createdAt"]).strftime("%d/%m/%Y")}\n'
+
+        return result
--- controllers/ServerController.py
+++ controllers/ServerController.py
@@ -1,9 +1,16 @@
 import requests
 
-class ServerController:
 
-    # BASE_URL = 'https://slack-taller2.herokuapp.com/bots/messages'
-    BASE_URL = 'http://localhost:3000/bots/messages'  #TODO remove this
+class ServerController:
+    # BASE_URL = 'https://slack-taller2.herokuapp.com'
+    BASE_URL = 'http://localhost:3000'  # TODO remove this
+    TIMEOUT = 180
 
     def send_response(self, response):
-        requests.post(self.BASE_URL, json=response.get_data(), timeout=180)
+        url = f'{self.BASE_URL}/bots/messages'
+        requests.post(url, json=response.get_data(), timeout=self.TIMEOUT)
+
+    def get_user(self, user_id):
+        url = f'{self.BASE_URL}/users/{user_id}'
+        response = requests.get(url, timeout=self.TIMEOUT)
+        return response.json()
