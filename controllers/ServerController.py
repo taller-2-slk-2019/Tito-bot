a1023405a3d283ac911e5fd9f40d809399cf06bd
matias feld <matias.feld@mdevz.com>
2019-04-26 22:47:20 -0300

    implemented error messages

--- controllers/ServerController.py
+++ controllers/ServerController.py
@@ -2,8 +2,8 @@ import requests
 
 class ServerController:
 
-    # BASE_URL = 'https://slack-taller2.herokuapp.com/'
-    BASE_URL = 'http://localhost:3000/'
+    # BASE_URL = 'https://slack-taller2.herokuapp.com/bots/messages'
+    BASE_URL = 'http://localhost:3000/bots/messages'
 
     def send_response(self, response):
-        return
+        requests.post(self.BASE_URL, json=response.get_data())
--- helpers/CommandResponse.py
+++ helpers/CommandResponse.py
@@ -3,3 +3,10 @@ class CommandResponse:
     def __init__(self, request, result):
         self.request = request
         self.result = result
+
+    def get_data(self):
+        return {
+            'channelId': self.request.channel_id,
+            'message': self.result,
+            'bot': self.request.bot_name
+        }
