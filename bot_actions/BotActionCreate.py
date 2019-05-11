2eca91655da878487f2c54c73dab0f5d2a0862e1
matias feld <matias.feld@mdevz.com>
2019-05-11 16:20:56 -0300

    updated channel creator

--- bot_actions/BotActionCreate.py
+++ bot_actions/BotActionCreate.py
@@ -4,9 +4,12 @@ from controllers.ServerController import ServerController
 class BotActionCreate:
 
     def channel_created(self, user_id):
-        user = ServerController().get_user(user_id)
+        if user_id:
+            name = ServerController().get_user(user_id)["name"]
+        else:
+            name = "El administrador"
 
-        result = f'{user["name"]} ha creado el canal'
+        result = f'{name} ha creado el canal'
         return result
 
     def conversation_created(self, user_id):
